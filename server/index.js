f653666b03c319b648303897b8933c3b94888256
Nikhil George <nikhilgeorge2015@gmail.com>
2023-07-23 16:19:15 +0530

    change: port + start script

--- server/index.js
+++ server/index.js
@@ -1,21 +1,21 @@
-const express = require("express")
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 
-const getCoreSectionsRouter = require("./controllers/getCoreSections")
-const getElectivesRouter = require("./controllers/getElectives")
-const getTimetableRouter = require("./controllers/getTimetable")
+const getCoreSectionsRouter = require("./controllers/getCoreSections");
+const getElectivesRouter = require("./controllers/getElectives");
+const getTimetableRouter = require("./controllers/getTimetable");
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001;
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use("/api", getCoreSectionsRouter)
-app.use("/api", getElectivesRouter)
-app.use("/api", getTimetableRouter)
+app.use("/api", getCoreSectionsRouter);
+app.use("/api", getElectivesRouter);
+app.use("/api", getTimetableRouter);
 
-app.listen(3001,() => {
-    console.log(`Server started on port ${PORT}`)
-})
+app.listen(3001, () => {
+  console.log(`Server started on port ${PORT}`);
+});
